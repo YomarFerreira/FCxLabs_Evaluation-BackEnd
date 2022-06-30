82dfd44ce8ac08c29a8c90a424d8d12e735806e7
YomarFerreira <yomar@ymail.com>
2022-06-29 21:24:58 -0300

    BackEnd Java Project Start with Spring Boot and Dependencies

---
+++ src/test/java/br/com/fcxlabs/project/test/ProjectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fcxlabs.project.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
